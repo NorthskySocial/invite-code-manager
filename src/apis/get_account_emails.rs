b25a055fc022da3de60a02209ad00b0604edd3dd
Ripperoni <email@ripperoni.com>
2026-03-09 17:40:41 -0700

    Fixed clippy issue

--- src/apis/get_account_emails.rs
+++ src/apis/get_account_emails.rs
@@ -4,8 +4,8 @@ use crate::error::AppError;
 use crate::user::InviteCodeAdmin;
 use axum::{Json, extract::Query, extract::State, response::IntoResponse};
 use serde::{Deserialize, Serialize};
-use utoipa::{IntoParams, ToSchema};
 use std::collections::HashMap;
+use utoipa::{IntoParams, ToSchema};
 
 #[derive(Deserialize, IntoParams, Debug)]
 pub struct AccountEmailsQuery {
@@ -56,18 +56,15 @@ pub async fn get_account_emails_handler(
         .get(config.pds_endpoint.clone() + GET_ACCOUNT_INFOS)
         .header("Content-Type", "application/json")
         .basic_auth("admin", Some(config.pds_admin_password.clone()));
-    
+
     for did in &query.dids {
         request = request.query(&[("dids", did)]);
     }
 
-    let res = request
-        .send()
-        .await
-        .map_err(|e| {
-            tracing::error!("Request for account infos failed: {}", e);
-            AppError::InternalError(e.to_string())
-        })?;
+    let res = request.send().await.map_err(|e| {
+        tracing::error!("Request for account infos failed: {}", e);
+        AppError::InternalError(e.to_string())
+    })?;
 
     if !res.status().is_success() {
         let status = res.status();
@@ -90,9 +87,7 @@ pub async fn get_account_emails_handler(
 
     // Ensure all requested DIDs are in the response, even if missing from PDS
     for did in query.dids {
-        if !emails.contains_key(&did) {
-            emails.insert(did, None);
-        }
+        emails.entry(did).or_insert(None);
     }
 
     Ok(Json(AccountEmailsResponse { emails }))
