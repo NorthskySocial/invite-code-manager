934bfa650b4b7009e0bd4cf1ec0f42e255e79a72
polaris <andres@northskysocial.com>
2026-09-22 14:25:29 -0700

    Clean unneeded properties from listAdmin response

--- src/apis/mod.rs
+++ src/apis/mod.rs
@@ -1,4 +1,4 @@
-use crate::user::{InviteCodeAdmin, InviteCodeAdminData};
+use crate::user::{InviteCodeAdmin, InviteCodeAdminData, InviteCodeAdminSummary};
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
@@ -74,6 +74,14 @@ pub fn invite_code_admin_to_response(user: &InviteCodeAdmin) -> InviteCodeAdminD
         username: user.username.to_owned(),
         otp_auth_url: user.otp_auth_url.to_owned(),
         otp_base32: user.otp_base32.to_owned(),
+        otp_enabled: user.otp_enabled.eq(&1),
+        otp_verified: user.otp_verified.eq(&1),
+    }
+}
+
+pub fn invite_code_admin_to_summary(user: &InviteCodeAdmin) -> InviteCodeAdminSummary {
+    InviteCodeAdminSummary {
+        username: user.username.to_owned(),
         otp_enabled: user.otp_enabled.eq(&1),
         otp_verified: user.otp_verified.eq(&1),
     }
